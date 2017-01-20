20b2a5468d5f11999f237488c6736d61d494ea5c
CodrinFaca <faca.ionut@gmail.com>
2017-01-20 05:43:37 +0200

    Edit wrong version

--- sudoku-tabu-search-master/src/exception/WrongSudokuSizeException.java
+++
@@ -1,9 +0,0 @@
-package exception;
-
-public class WrongSudokuSizeException extends Exception {
-	private static final long serialVersionUID = 1L;
-
-	public WrongSudokuSizeException(String msg) {
-		super(msg);
-	}
-}
